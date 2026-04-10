8b6834576bcea329b55e15289fa8391cf1b1d04b
Myer <19848162+myerDOT@users.noreply.github.com>
2026-04-09 20:45:56 -0400

    why import problem

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,2 +1,2 @@
 export { prisma } from "./client.ts"; // exports instance of prisma
-export * from "../generated/prisma/client.ts"; // exports generated types from prisma
+export * as Prisma from "../generated/prisma/internal/prismaNamespace.ts";
