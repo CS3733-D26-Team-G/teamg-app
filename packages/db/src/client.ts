b2091eb5277f9b4a2e891c22bb5bc2dfc1558ba4
Myer <19848162+myerDOT@users.noreply.github.com>
2026-04-14 18:47:27 -0400

    use correct env variable

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,11 +1,12 @@
+import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 // this code gets imported from backend, so the env variable must be SUPABASE_URL, not DATABASE_URL
 // don't ask me how to fix this or why. it works so im not gonna touch it
-const connectionString = process.env.SUPABASE_URL;
+const connectionString = process.env.DATABASE_URL;
 if (!connectionString) {
-  throw new Error("SUPABASE_URL environment variable must be set!");
+  throw new Error("DATABASE_URL environment variable must be set!");
 }
 
 const adapter = new PrismaPg({ connectionString });
